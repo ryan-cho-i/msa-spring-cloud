133754e06519678a8d6edf6b34fd2f32b9fcaf42
ryan-cho-i <chlqudtn347@gmail.com>
2024-09-16 17:37:23 -0400

    fix config

---
+++ user-service/src/main/java/org/ryanchoi/user_service/security/AuthenticationFilter.java
@@ -0,0 +1,7 @@
+package org.ryanchoi.user_service.security;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class AuthenticationFilter  extends UsernamePasswordAuthenticationFilter {
+
+}
