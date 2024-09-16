4f7a3d1b6236bdc81adfdddd3a5223066d5f4df9
ryan-cho-i <chlqudtn347@gmail.com>
2024-09-16 17:50:43 -0400

    User Micro - Authenticaiton 추가

--- user-service/src/main/java/org/ryanchoi/user_service/security/WebSecurity.java
+++ user-service/src/main/java/org/ryanchoi/user_service/security/WebSecurity.java
@@ -5,28 +5,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity {
-    private static final String[] WHITE_LIST = {
-            "/users/**",
-            "/**"
-    };
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+//    private static final String[] WHITE_LIST = {
+//            "/users/**",
+//            "/**"
+//    };
+//
+//    @Bean
+//    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                  .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+//                .authorizeHttpRequests(
+//                        authorize -> authorize
+//                                .requestMatchers(WHITE_LIST).permitAll()
+//                                .requestMatchers( PathRequest.toH2Console() ).permitAll()
+//                )
+//                .build();
+//    }
 
-    @Bean
-    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .requestMatchers(WHITE_LIST).permitAll()
-                                .requestMatchers( PathRequest.toH2Console() ).permitAll()
-                )
-                .build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.0.8")
+                .and()
+                .addFilter(getAuthenticationFilter());
+
+        http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
