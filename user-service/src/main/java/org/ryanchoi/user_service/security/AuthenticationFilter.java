ae56f39b987ab3e468851b0c043fffab955e0717
ryan-cho-i <chlqudtn347@gmail.com>
2024-09-17 16:51:02 -0400

    Fixing

--- user-service/src/main/java/org/ryanchoi/user_service/security/AuthenticationFilter.java
+++ user-service/src/main/java/org/ryanchoi/user_service/security/AuthenticationFilter.java
@@ -5,7 +5,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.ryanchoi.user_service.service.UserService;
 import org.ryanchoi.user_service.vo.RequestLogin;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -15,6 +18,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class AuthenticationFilter  extends UsernamePasswordAuthenticationFilter {
+
+    private UserService userService;
+    private Environment environment;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager,
+                                UserService userService,
+                                Environment environment) {
+        super(authenticationManager);
+        this.userService = userService;
+        this.environment = environment;
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response
@@ -38,7 +53,7 @@ public class AuthenticationFilter  extends UsernamePasswordAuthenticationFilter 
     protected void successfulAuthentication(HttpServletRequest request,
                                             HttpServletResponse response,
                                             FilterChain chain,
-                                            Authentication authResult
+                                            Authentication auth
     ) throws IOException, ServletException {
     }
 }
--- user-service/src/main/java/org/ryanchoi/user_service/security/WebSecurity.java
+++ user-service/src/main/java/org/ryanchoi/user_service/security/WebSecurity.java
@@ -1,51 +1,89 @@
 package org.ryanchoi.user_service.security;
 
+import org.ryanchoi.user_service.service.UserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-//    private static final String[] WHITE_LIST = {
-//            "/users/**",
-//            "/**"
-//    };
-//
-//    @Bean
-//    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                  .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-//                .authorizeHttpRequests(
-//                        authorize -> authorize
-//                                .requestMatchers(WHITE_LIST).permitAll()
-//                                .requestMatchers( PathRequest.toH2Console() ).permitAll()
-//                )
-//                .build();
-//    }
+public class WebSecurity {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("192.168.0.8")
-                .and()
-                .addFilter(getAuthenticationFilter());
+    private static final String MY_IP = "74.102.125.219";
 
-        http.headers().frameOptions().disable();
+    private static final String[] WHITE_LIST_GET = {
+            "/actuator/**",
+            "/h2-console/**",
+            "/welcome",
+            "/health-check",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/v3/api-docs/**"
+    };
+
+    private static final String[] WHITE_LIST_POST = {
+            "/users"
+    };
+
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment env) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.env = env;
     }
 
-    private AuthenticationFilter getAuthenticationFilter() throws Exception {
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
-        authenticationFilter.setAuthenticationManager(authenticationManager());
+//    @Bean
+//    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+////        AuthenticationManagerBuilder authenticationManagerBuilder =
+////                http.getSharedObject(AuthenticationManagerBuilder.class);
+////        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+////        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+//
+//        http.csrf( (csrf) -> csrf.disable() );
+//
+//        http.authorizeHttpRequests(
+//                authorize -> authorize
+//                        .requestMatchers(WHITE_LIST_GET).permitAll()
+//                        .requestMatchers(HttpMethod.POST, WHITE_LIST_POST).permitAll()
+//                        .requestMatchers("/**").access(
+//                                new WebExpressionAuthorizationManager("hasIpAddress('localhost') or hasIpAddress('127.0.0.1') or hasIpAddress('" + MY_IP + "')"))
+//                        .anyRequest().authenticated()
+//        );
+//
+//        http.headers((headers) -> headers.frameOptions( (frameOptions) -> frameOptions.sameOrigin()));
+//
+//        return http.build();
+//    }
 
-        return authenticationFilter;
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                  .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers(WHITE_LIST_GET).permitAll()
+                                .requestMatchers( PathRequest.toH2Console() ).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+        return new AuthenticationFilter(authenticationManager, userService, env);
     }
 }
